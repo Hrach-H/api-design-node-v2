de6f59c55ab852cba2bc40f9a348671cf9635a30
hrachya.hovsepyan <hrach.hovsepyan@gmail.com>
2018-03-19 15:46:31 +0400

    Middleware

--- src/api/modules/auth.js
+++ src/api/modules/auth.js
@@ -3,34 +3,34 @@ import jwt from 'jsonwebtoken'
 import expressJwt from 'express-jwt'
 const jwtSecret = 'blueRhinoJumps'
 
-const checkToken = expressJwt({ secret: jwtSecret })
-const disableAuth = false
+const checkToken = expressJwt({ secret: jwtSecret });
+const disableAuth = false;
 
 export const signin = (req, res, next) => {
   // req.user will be there from the middleware
   // verify user. Then we can just create a token
   // and send it back for the client to consume
-  const token = signToken(req.user.id)
-  res.json({token: token})
-}
+  const token = signToken(req.user.id);
+  res.json({token: token});
+};
 
 export const decodeToken = () => (req, res, next) => {
   if (disableAuth) {
-    return next()
+    return next();
   }
   // make it optional to place token on query string
   // if it is, place it on the headers where it should be
   // so checkToken can see it. See follow the 'Bearer 034930493' format
   // so checkToken can see it and decode it
   if (req.query && req.query.hasOwnProperty('access_token')) {
-    req.headers.authorization = 'Bearer ' + req.query.access_token
+    req.headers.authorization = 'Bearer ' + req.query.access_token;
   }
 
   // this will call next if token is valid
   // and send error if its not. It will attached
   // the decoded token to req.user
-  checkToken(req, res, next)
-}
+  checkToken(req, res, next);
+};
 
 export const getFreshUser = () => (req, res, next) => {
   return User.findById(req.user.id)
@@ -45,21 +45,21 @@ export const getFreshUser = () => (req, res, next) => {
       } else {
         // update req.user with fresh user from
         // stale token data
-        req.user = user
-        next()
+        req.user = user;
+        next();
       }
     })
     .catch(error => next(error))
-}
+};
 
 export const verifyUser = () => (req, res, next) => {
-  const username = req.body.username
-  const password = req.body.password
+  const username = req.body.username;
+  const password = req.body.password;
 
   // if no username or password then send
   if (!username || !password) {
-    res.status(400).send('You need a username and password')
-    return
+    res.status(400).send('You need a username and password');
+    return;
   }
 
   // look user up in the DB so we can check
@@ -67,28 +67,28 @@ export const verifyUser = () => (req, res, next) => {
   User.findOne({username: username})
     .then(function(user) {
       if (!user) {
-        res.status(401).send('No user with the given username')
+        res.status(401).send('No user with the given username');
       } else {
         // checking the passowords here
         if (!user.authenticate(password)) {
-          res.status(401).send('Wrong password')
+          res.status(401).send('Wrong password');
         } else {
           // if everything is good,
           // then attach to req.user
           // and call next so the controller
           // can sign a token from the req.user._id
           req.user = user;
-          next()
+          next();
         }
       }
     })
-    .catch(error => next(err))
-}
+    .catch(error => next(err));
+};
 
 export const signToken = (id) => jwt.sign(
   {id},
   jwtSecret,
   {expiresIn: '30d'}
-)
+);
 
-export const protect = [decodeToken(), getFreshUser()]
+export const protect = [decodeToken(), getFreshUser()];
--- src/api/modules/errorHandler.js
+++ src/api/modules/errorHandler.js
@@ -1,3 +1,4 @@
 export const apiErrorHandler = (error, req, res, next) => {
-  
-}
+    console.log('Error: ', error);
+    res.status(500).send('Oops');
+};
