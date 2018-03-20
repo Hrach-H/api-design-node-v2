6bd88e4f8708f9728c7f7639b933e55a9393cd57
hrachya.hovsepyan <hrach.hovsepyan@gmail.com>
2018-03-20 10:40:57 +0400

    Added Schemas

--- src/api/resources/playlist/playlist.model.js
+++ src/api/resources/playlist/playlist.model.js
@@ -1,9 +1,24 @@
 import mongoose from 'mongoose'
 
 export const schema = {
-  
-}
+    title: {
+        type: String,
+        required: [true, 'A playlist should have a title']
+    },
+    songs: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'song',
+            required: true,
+        }
+    ],
+    favorite: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+};
 
-const playlistSchema = new mongoose.Schema(schema)
+const playlistSchema = new mongoose.Schema(schema);
 
-export const Playlist = mongoose.model('playlist', playlistSchema)
+export const Playlist = mongoose.model('playlist', playlistSchema);
--- src/api/resources/song/song.model.js
+++ src/api/resources/song/song.model.js
@@ -1,9 +1,30 @@
 import mongoose from 'mongoose'
 
 export const schema = {
-  
-}
+    title: {
+      type: String,
+      required: [true, 'A song has to have a title']
+    },
+    url: {
+        type: String,
+        required: [true, 'Please, provide a url for the song'],
+        unique: true
+    },
+    album: String,
+    artist: String,
+    rating: {
+        type: Number,
+        min: 0,
+        max: 5,
+        default: 0
+    },
+    favorite: {
+        type: Boolean,
+        required: true,
+        default: false,
+    }
+};
 
-const songSchema = new mongoose.Schema(schema)
+const songSchema = new mongoose.Schema(schema);
 
-export const Song = mongoose.model('song', songSchema)
+export const Song = mongoose.model('song', songSchema);
